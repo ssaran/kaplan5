5b1ce055e88c60b16c12ee5d3cf7d5843a7b882b
www-data <www-data@devel2.ncn.int>
2023-08-30 10:25:24 +0300

    Auth exception for auth error handling

---
+++ src/Exception/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace K5\Exception;
+
+class Auth extends \Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, \Throwable $previous = null) {
+        // some code
+
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
+
+    // custom string representation of object
+    public function __toString() : string
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
