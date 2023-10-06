e330ce68267bb1f839b542ada1c12d40d0f62937
www-data <www-data@devel2.ncn.int>
2023-10-06 14:09:31 +0300

    Task base refactor

--- src/Unity/Phalcon/Task/TaskBase.php
+++ src/Unity/Phalcon/Task/TaskBase.php
@@ -11,43 +11,17 @@ namespace K5\Unity\Phalcon\Task;
 
 use K5\U as u;
 
-class TaskBase extends \Phalcon\Mvc\Application
+class TaskBase extends \Phalcon\Cli\Task
 {
     const KEY_CONN = "conn_";
     const KEY_TIMER = "timer_";
     const WS_PING_DELAY = 5000;
 
-    public $redisCache;
-
-    protected $config;
-    protected $di;
     /** @var \K5\Entity\Request\Setup */
     protected \K5\Entity\Request\Setup $setup;
-    protected $token;
-    protected $domain;
-    protected $companyAccountId;
 
-    protected $companyConfig;
-    /** @var \Xrp\Common\Entity\Auth\FrontUser */
-    protected $frontUser;
-
-    public function __construct()
+    public function initialize()
     {
-        parent::__construct();
-        $this->setup();
-    }
 
-    public function GetConfig()
-    {
-        return $this->config;
-    }
-
-    protected function setup()
-    {
-        $this->config = $this->getDI()->get('config');
-        $this->di = $this->getDI();
-
-        $cfg = new \Common\Config\Api();
-        $this->config->xrp = $cfg->config;
     }
 }
