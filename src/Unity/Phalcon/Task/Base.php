f64b4d7284d00580f4b7330ded73980abfc4e655
www-data <www-data@devel2.ncn.int>
2024-12-23 13:39:36 +0300

    Task update v2

--- src/Unity/Phalcon/Task/Base.php
+++ src/Unity/Phalcon/Task/Base.php
@@ -18,11 +18,33 @@ class Base extends \Phalcon\Cli\Task
 
     /** @var \K5\Entity\Request\Setup */
     protected \K5\Entity\Request\Setup $setup;
+    protected ?string $domain = null;
+    protected ?string $subDomain = null;
     protected ?array $params = null;
 
     public function initialize()
     {
 
+    }
+
+    public function SetDomain(string $domain) : void
+    {
+        $this->domain = $domain;
+    }
+
+    public function SetSubDomain(string $subDomain) : void
+    {
+        $this->subDomain = $subDomain;
+    }
+
+    public function GetDomain() : ?string
+    {
+        return $this->domain;
+    }
+
+    public function GetSubDomain() : ?string
+    {
+        return $this->subDomain;
     }
 
     public function AddParams(string $param) : void
